14641a57b8d12bc5ae3aef0aa47e98f13b683876
Hasin Raihan Shovon <raihanshovon95@gmail.com>
2021-02-28 21:59:24 +0600

    Tokenizer test passed on both single file and directory, can produce errorless output file

--- syntax-analyzer/Tokenizer.java
+++ syntax-analyzer/Tokenizer.java
@@ -9,6 +9,15 @@ public class test{
 		Scanner sc2=null;
 		int flag=0,brk=0;
 		String temp="";
+
+		String name=args[0];
+		File dirpath=new File(name);
+		File fileList[];
+		String[] outname;
+		int dir=0;
+		BufferedWriter out=null;
+		String outfile=null;
+
 		Map<String,String> map=new HashMap<String,String>();
 
 		map.put("class","keyword");
@@ -53,81 +62,138 @@ public class test{
 		map.put("=","symbol");
 		map.put("~","symbol");
 
-		try{
-			sc=new Scanner(new File(args[0])).useDelimiter(" |,");
-		}catch(FileNotFoundException e){
-			System.out.println("File not found\n");
+		if(!dirpath.isDirectory()){
+			//System.out.println("it is a single file, not a directory");
+			fileList=new File[1];
+			fileList[0]=dirpath;
+			outname= name.split("[.]");
+		}
+		else{
+			fileList= dirpath.listFiles();
+			dir=1;
+			outname=new String[1];
+			outname[0]=name;
 		}
 
-		while(sc.hasNextLine()){
-			String st=sc.nextLine();
-			if(!st.equals("")){
-				char ch=st.charAt(0);
-				char ch2;
-				try{
-					ch2=st.charAt(1);
-				}catch(StringIndexOutOfBoundsException s){
-					ch2=' ';
-				}
-				if(ch=='/' || (ch==' ' && ch2=='*')){
-					continue;
+		for(File f : fileList){
+			String[] fname=f.getName().split("[.]");
+			if(fname[1].equals("jack")){
+				if(dir==0){
+					outfile=outname[0]+".xml";
 				}
 				else{
-					sc2=new Scanner(st);
-					while(sc2.hasNext()){
-						if(brk==1){
-							brk=0;
-							break;
+					outfile=outname[0]+"/"+fname[0]+".xml";
+				}
+
+				try{
+					out = new BufferedWriter(new FileWriter(outfile));
+					out.write("<tokens>\n");
+				}catch(IOException k){
+					System.out.println(outfile + " ->Couldn't open the output file");
+				}
+
+				try{
+					sc=new Scanner(f);
+					System.out.println(f.getName()+"OPENED");
+				}catch(FileNotFoundException e){
+					System.out.println(f.getName()+" ->File not found\n");
+				}
+
+				while(sc.hasNextLine()){
+					String st=sc.nextLine();
+					if(!st.equals("")){
+						char ch=st.charAt(0);
+						char ch2;
+						try{
+							ch2=st.charAt(1);
+						}catch(StringIndexOutOfBoundsException s){
+							ch2=' ';
 						}
-						String token=sc2.next();
-						String[] str= token.split("(?<=[~.;,\\[\\])(\"])|(?=[~.;,\\[\\])(\"])"); 
+						if(ch=='/' || (ch==' ' && ch2=='*')){
+							continue;
+						}
+						else{
+							sc2=new Scanner(st);
+							while(sc2.hasNext()){
+								if(brk==1){
+									brk=0;
+									break;
+								}
+								String token=sc2.next();
+								String[] str= token.split("(?<=[~.;,\\[\\])(\"])|(?=[~.;,\\[\\])(\"])"); 
 
-						for(int i=0;i<str.length;i++){
+								for(int i=0;i<str.length;i++){
 
-							String val=map.get(str[i]);
+									String val=map.get(str[i]);
 
-							if(val==null){
-								//System.out.println(str[i]+"->Not included in the map\n");
-								int num=str[i].charAt(0);
-								if(str[i].charAt(0)=='/'){
-									brk=1;
-									continue;
-								}
-								if(num>=48 && num<=57){
-									//int integer=Integer.valueOf(str[i]);
-									val="integerConstant";
-									System.out.println("<"+val+">"+str[i]+"</"+val+">");
-								}
+									if(val==null){
+										//System.out.println(str[i]+"->Not included in the map\n");
+										int num=str[i].charAt(0);
+										if(str[i].charAt(0)=='/'){
+											brk=1;
+											continue;
+										}
+										if(num>=48 && num<=57){
+											val="integerConstant";
+											//System.out.println("<"+val+">"+str[i]+"</"+val+">");
+											try{
+												out.write("<"+val+">"+str[i]+"</"+val+">\n");
+											}catch(IOException k){
+												System.out.println("Something is Wrong");
+											}
+										}
 
-								else if(str[i].equals("\"") && flag==0){
-									flag=1;
-									temp="";
+										else if(str[i].equals("\"") && flag==0){
+											flag=1;
+											temp="";
+										}
+										else if(str[i].equals("\"") && flag==1){
+											flag=0;
+											//System.out.println("<stringConstant>"+temp+"</stringConstant>");
+											try{
+												out.write("<stringConstant>"+temp+"</stringConstant>\n");
+											}catch(IOException k){
+												System.out.println("Something is Wrong");
+											}
+										}
+										else if(flag==1){
+											temp+=str[i];
+										}
+										else{
+											//System.out.println("<identifier>"+str[i]+"</identifier>");
+											try{
+												out.write("<identifier>"+str[i]+"</identifier>\n");
+											}catch(IOException k){
+												System.out.println("Something is Wrong");
+											}
+										}
+									}
+									else{
+										try{
+											if(str[i].equals("<"))
+												out.write("<"+val+">"+"&lt;"+"</"+val+">\n");
+											else if(str[i].equals(">"))
+												out.write("<"+val+">"+"&gt;"+"</"+val+">\n");
+											else if(str[i].equals("&"))
+												out.write("<"+val+">"+"&amp;"+"</"+val+">\n");
+											else if(str[i].equals("\""))
+												out.write("<"+val+">"+"&quot;"+"</"+val+">\n");
+											else
+												out.write("<"+val+">"+str[i]+"</"+val+">\n");
+										}catch(IOException k){
+											System.out.println("Something is Wrong");
+										}
+									}
 								}
-								else if(str[i].equals("\"") && flag==1){
-									flag=0;
-									System.out.println("<stringConstant>"+temp+"</stringConstant>");
-								}
-								else if(flag==1){
-									temp+=str[i];
-								}
-								else{
-									System.out.println("<identifier>"+str[i]+"</identifier>");
-								}
-							}
-							else{
-								if(str[i].equals("<"))
-									System.out.println("<"+val+">"+"&lt;"+"</"+val+">");
-								else if(str[i].equals(">"))
-									System.out.println("<"+val+">"+"&gt;"+"</"+val+">");
-								else if(str[i].equals("&"))
-									System.out.println("<"+val+">"+"&amp;"+"</"+val+">");
-								else if(str[i].equals("\""))
-									System.out.println("<"+val+">"+"&quot;"+"</"+val+">");
-								else
-									System.out.println("<"+val+">"+str[i]+"</"+val+">");
 							}
 						}
 					}
+				}
+				try{
+					out.write("</tokens>");
+					out.close();
+				}catch(IOException k){
+					System.out.println("File didn't close properly");
 				}
 			}
 		}
