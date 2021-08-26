1d99d314a69ead70922abeb00cfd5c9a28212058
r8025n <raihanshovon95@gmail.com>
2021-08-26 20:18:42 +0600

    Refactor JackAnalyzer.java

--- syntax-analyzer/JackAnalyzer.java
+++ syntax-analyzer/JackAnalyzer.java
@@ -3,12 +3,12 @@ import java.io.*;
 import java.lang.String;
 
 public class JackAnalyzer{
-	public static void main(String args[]){
-		String name=args[0];
-		File dirpath=new File(name);
 
-		CompilationEngine engine=new CompilationEngine(name);
-		engine.mapInitialize();
-		engine.parser();
+	public static void main(String args[]) {
+		String fileName = args[0];
+		File dirpath = new File(fileName);
+		CompilationEngine newSyntaxAnalyzer = new CompilationEngine(fileName);
+		newSyntaxAnalyzer.mapInitialize();
+		newSyntaxAnalyzer.parser();
 	}
 }
